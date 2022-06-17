f807958e79523b770896d904efff93b4fb68335e
유현정/(협력사) 과장/클라우드플랫폼(연) 클라우드플랫폼개발팀(hyunjung.ryu@lgepartner.com) <hyunjung.ryu@lge.com>
2022-06-17 15:26:16 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './assets/scss/otr.scss';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ClippedDrawer from './components/ClippedDrawer';
+import Box from '@mui/material/Box';
+import Breadcrumbs from "./components/Breadcrumbs";
+import Contents from "./components/Contents";
+
+
+const theme = createTheme({
+  status: {
+    danger: '#D00202',
+  },
+  palette: {
+    primary: {
+      light: '#7879F1',
+      main: '#5D5FEF',
+      darker: '#4A45BD',
+      
+    },
+    secondary: {
+      light: '#7879F1',
+      main: '#7879F1',
+      darker: '#7879F1',
+    },
+    success: {
+      main: '#4A45BD',
+    }, 
+    search: {
+      light: '#694DE8',
+      main: '#8367FF',
+      darker: '#694DE8',
+      contrastText: '#ffffff',
+    }
+  },  
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme} className="App">
+      <Box sx={{ display: 'flex' }}>
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              ThinQ SOTA Admin
+            </Typography>
+            <Button color="inherit">Request as User</Button>
+            <Button color="inherit">Logout</Button>
+          </Toolbar>
+        </AppBar>
+        <ClippedDrawer/>
+        <Box component="main" className="container-fluid">
+          <Toolbar />
+          <Breadcrumbs/>
+          <Contents/>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
-
 export default App;
