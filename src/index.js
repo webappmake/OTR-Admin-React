8f763ff60f972229917041d742313f74fc62c443
유현정/(협력사) 차장/클라우드플랫폼(연) 클라우드플랫폼개발팀(hyunjung.ryu@lgepartner.com) <hyunjung.ryu@lge.com>
2022-11-09 10:09:05 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import AdminPortal from './AdminPortal';
+import AdminPortal from './AdminPortal';
 // import IntellyticsDashboard from './IntellyticsDashboard';
-import CloudDevelopersDay from './view/CloudDevelopersDay';
+// import CloudDevelopersDay from './view/CloudDevelopersDay';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <AdminPortal/> */}
+    <AdminPortal/>
     {/* <IntellyticsDashboard/> */}
-    <CloudDevelopersDay/>
+    {/* <CloudDevelopersDay/> */}
   </React.StrictMode>
 );
